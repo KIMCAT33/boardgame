a09ca6ef46f9ab063ed888d29638a531cb9895ca
jdkim <jdkim@amazon.com>
2021-10-18 22:59:09 +0900

    AWS Hello

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+# This is punishment to make simple helloworld code, 
+# But I'm winnder and loser at the same time. 
+# I chose Python as language to program "Hello World".
+
+def helloworld():
+    print("Hello World!")
+
+helloworld()
+
